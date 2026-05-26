d3725042dec0192b27ba7c0e098e74c7a823ce16
Kamil Kwapisz <Kamilos0428@gmail.com>
2026-05-26 12:14:51 +0200

    add new  files

--- Projekt_strona/test.py
+++
@@ -1,8 +0,0 @@
-liczby = [1, 5, 7, 15, 20, 49, 60];
-szukana = int(input("podaj liczbę do wyszukania: "));
-
-for i in range(len(liczby)):
-    if liczby[i] == szukana:
-        print(f"liczba {szukana} jest w zbiorze liczb");
-    else:
-        print("liczby nie znaleziono");
